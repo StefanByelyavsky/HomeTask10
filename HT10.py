a3a4282fc1a970de935f42c34fcee7a8d0895c33
Home <https://github.com/StefanByelyavsky/HT.git>
2022-02-22 09:26:40 +0200

    HomeTask10

--- HT10.py
+++ HT10.py
@@ -1,13 +1,12 @@
 z = 'yes'
 while z == 'yes' or z == 'y':
     a = input('задайте действие :')
-    print(a)
     b = list(a)
     if b[-2] == "/" and b[-1] == '0':
         print('На ноль делить нельзя')
     else:
-        c = eval(a)
-        print(c)
+        a = eval(a)
+        print(a)
     z = input('Желаете продолжить? (Yes\\No):').lower().strip()
     while z != 'yes' and z != 'no' and z != 'y' and z != 'n':
         print('введите корректный ответ')
